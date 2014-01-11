4b47f6975f329dfa22913863c71c272b527d5310
Rene Morales <rene.morales.sanchez@gmail.com>
2014-01-10 22:44:29 -0300

    COOKIE PANIC

--- tracker.js
+++ tracker.js
@@ -5,6 +5,9 @@ if ( typeof jQuery !== 'undefined' && typeof $ == 'undefined' ) { $=jQuery; }
 // jQuery Cookie Plugin
 (function(e){if(typeof define==="function"&&define.amd){define(["jquery"],e)}else{e(jQuery)}})(function(e){function n(e){return u.raw?e:encodeURIComponent(e)}function r(e){return u.raw?e:decodeURIComponent(e)}function i(e){return n(u.json?JSON.stringify(e):String(e))}function s(e){if(e.indexOf('"')===0){e=e.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,"\\")}try{e=decodeURIComponent(e.replace(t," "));return u.json?JSON.parse(e):e}catch(n){}}function o(t,n){var r=u.raw?t:s(t);return e.isFunction(n)?n(r):r}var t=/\+/g;var u=e.cookie=function(t,s,a){if(s!==undefined&&!e.isFunction(s)){a=e.extend({},u.defaults,a);if(typeof a.expires==="number"){var f=a.expires,l=a.expires=new Date;l.setTime(+l+f*864e5)}return document.cookie=[n(t),"=",i(s),a.expires?"; expires="+a.expires.toUTCString():"",a.path?"; path="+a.path:"",a.domain?"; domain="+a.domain:"",a.secure?"; secure":""].join("")}var c=t?undefined:{};var h=document.cookie?document.cookie.split("; "):[];for(var p=0,d=h.length;p<d;p++){var v=h[p].split("=");var m=r(v.shift());var g=v.join("=");if(t&&t===m){c=o(g,s);break}if(!t&&(g=o(g))!==undefined){c[m]=g}}return c};u.defaults={};e.removeCookie=function(t,n){if(e.cookie(t)===undefined){return false}e.cookie(t,"",e.extend({},n,{expires:-1}));return!e.cookie(t)}})
 
+// Editar este número para reiniciar cookies.
+var rctr_tmstmp = '100120142242';
+
 rctr = {
 	log: function(m){
 		if ( rctr.st.debug == true ) {
@@ -20,6 +23,7 @@ rctr = {
 		reading: false,
 		sightings: [],
 		renders: [],
+		cookies: { user: 'ReactorUserToken'+rctr_tmstmp, visit: 'ReactorVisitToken'+rctr_tmstmp }
 		cookieOpt: {path: '/', expires: 1000},
 		ready: false,
 		debug: false
@@ -58,14 +62,14 @@ rctr = {
 		getUser: function(){
 			rctr.log('[rctr] Catching user, looking for cookie.');
 			// Check for user token.
-			if ( $.cookie('ReactorUserToken') ) {
-				rctr.st.user_token = $.cookie('ReactorUserToken');
+			if ( $.cookie(rctr.st.cookies.user) ) {
+				rctr.st.user_token = $.cookie(rctr.st.cookies.user);
 				rctr.log('[rctr] Already found user '+rctr.st.user_token+'');
 				rctr.sess.getVisit();
 			} else {
 				$.get(rctr.st.server+'/users/create?access_token='+rctr.st.access_token, function(d){
 					rctr.st.user_token = d.user_token;
-					$.cookie('ReactorUserToken', d.user_token, rctr.st.cookieOpt);
+					$.cookie(rctr.st.cookies.user, d.user_token, rctr.st.cookieOpt);
 					rctr.log('[rctr] Created new user '+rctr.st.user_token+'');
 					rctr.sess.getVisit();
 				}, 'json');
@@ -75,8 +79,8 @@ rctr = {
 			var no_token = true;
 			rctr.log('[rctr] Catching visit, looking for cookie.');
 			// Check for visit token.
-			if ( $.cookie('ReactorVisitToken') ) {
-				var visit = $.cookie('ReactorVisitToken').split('.');
+			if ( $.cookie(rctr.st.cookies.user) ) {
+				var visit = $.cookie(rctr.st.cookies.user).split('.');
 				var this_time = Date.now();
 				var visit_time = parseInt(visit[1]);
 				if ( this_time-visit_time < 600000 ) {		// Si han pasado menos de 10 minutos, continuar visita.
@@ -93,7 +97,7 @@ rctr = {
 				$.get(rctr.st.server+'/visits/create?access_token='+rctr.st.access_token+'&user_token='+rctr.st.user_token, function(d){
 					rctr.st.visit_token = d.visit_token;
 					rctr.log('[rctr] Created new visit '+rctr.st.visit_token);
-					$.cookie('ReactorVisitToken', d.visit_token+'.'+Date.now(), rctr.st.cookieOpt);
+					$.cookie(rctr.st.cookies.user, d.visit_token+'.'+Date.now(), rctr.st.cookieOpt);
 					rctr.st.ready = true;
 					rctr.track();
 				}, 'json');
@@ -101,10 +105,10 @@ rctr = {
 		},
 		updateVisitTime: function(){
 			// Update last seen of Visit.
-			var visit = $.cookie('ReactorVisitToken').split('.');
+			var visit = $.cookie(rctr.st.cookies.user).split('.');
 			visit[1] = Date.now();
 			visit = visit.join('.');
-			$.cookie('ReactorVisitToken', visit, rctr.st.cookieOpt);
+			$.cookie(rctr.st.cookies.user, visit, rctr.st.cookieOpt);
 		}
 	},
 	comm: {
@@ -177,8 +181,8 @@ rctr = {
 	},
 	dbg: {
 		killCookies: function(){
-			$.removeCookie('ReactorUserToken', rctr.st.cookieOpt);
-			$.removeCookie('ReactorVisitToken', rctr.st.cookieOpt);
+			$.removeCookie(rctr.st.cookies.user, rctr.st.cookieOpt);
+			$.removeCookie(rctr.st.cookies.user, rctr.st.cookieOpt);
 		}
 	},
 	track: function(){
