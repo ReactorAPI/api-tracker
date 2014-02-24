ddf87622328b8964abf976086c68e9bc05a66bf4
Rene Morales <rene.morales.sanchez@gmail.com>
2014-02-24 15:54:39 -0300

    Prevent constant pinging

--- parser.soychile.js
+++ parser.soychile.js
@@ -6,20 +6,27 @@ if ( typeof jQuery !== 'undefined' && typeof $ == 'undefined' ) { $=jQuery; }
 
 function rctr_convert_articles(){
 	if ( typeof rctr !== 'undefined' && rctr.st.ready == true ) {
+		var p = false;
+
 		$('[data-idnoticia]').not('[data-reactor-articleid]').each(function(){
 			var id = $(this).attr('data-idnoticia');
 			$(this).attr('data-reactor-articleid', id);
+			p = true;
 		});
 
 		var split_url = location.href.split('/');
 		if ( split_url.length == 10 ) {
 			if ( split_url[8].length == 6 ) {
 				// Asumamos que es un artículo.
-				$('#content #note-inside').attr('data-reactor-articleid', split_url[8]).attr('data-reactor-ismainarticle', 'true');
+				if ( $('#content #note-inside[data-reactor-ismainarticle]').length == 0 ) {
+					$('#content #note-inside').attr('data-reactor-articleid', split_url[8]).attr('data-reactor-ismainarticle', 'true');
+					p = true;
+				}	
 			}
 		}
-
-		rctr.track();
+		if ( p == true ) {
+			rctr.track();
+		}
 	}
 
 	setTimeout(rctr_convert_articles, 3000);
